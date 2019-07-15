d36c9eee31de5436916d8ed00df664e5196942cd
Abhilash Sanne <sanne.abhi@gmail.com>
2019-07-15 21:50:02 +0530

    Add Loops example implementations

---
+++ Day1/loops/for.py
@@ -0,0 +1,9 @@
+
+# For loop
+print('For Loop Execution')
+for i in range(0, 10):
+    print(i)
+
+print('For Loop Execution2')
+for i in range(10):
+    print(i * 10)
---
+++ Day1/loops/while.py
@@ -0,0 +1,8 @@
+# while iteration
+print('while Loop Execution')
+
+j = 10
+i = 0
+while j > i:
+    print(1 + i)
+    i += 1
