680145c9e0720aa305c0135ddd90aa53f7b293dc
Abhilash Sanne <sanne.abhi@gmail.com>
2019-07-15 21:49:27 +0530

    Add conditions examples

---
+++ Day1/conditions/functionMappintSwitcherr.py
@@ -0,0 +1,70 @@
+def one():
+    return "January"
+
+
+def two():
+    return "February"
+
+
+def three():
+    return "March"
+
+
+def four():
+    return "April"
+
+
+def five():
+    return "May"
+
+
+def six():
+    return "June"
+
+
+def seven():
+    return "July"
+
+
+def eight():
+    return "August"
+
+
+def nine():
+    return "September"
+
+
+def ten():
+    return "October"
+
+
+def eleven():
+    return "November"
+
+
+def twelve():
+    return "December"
+
+
+def numbers_to_months(argument):
+    switcher = {
+        1: one,
+        2: two,
+        3: three,
+        4: four,
+        5: five,
+        6: six,
+        7: seven,
+        8: eight,
+        9: nine,
+        10: ten,
+        11: eleven,
+        12: twelve
+    }
+    # Get the function from switcher dictionary
+    return switcher.get(argument, "Invalid month")
+    # Execute the function
+
+
+func = numbers_to_months(4)
+print(func())
