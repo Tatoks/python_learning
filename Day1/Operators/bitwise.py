e8003a2b567561ffc54b716d0988dba5c22da357
Abhilash Sanne <sanne.abhi@gmail.com>
2019-07-15 20:51:46 +0530

    Add Operators examples

---
+++ Day1/Operators/bitwise.py
@@ -0,0 +1,31 @@
+# Bitwise Operator & | ^
+
+print('Bitwise AND operator')
+print(0 & 0)
+print(0 & 1)
+print(1 & 0)
+print(1 & 1)
+
+print('Bitwise OR operator')
+print(0 | 0)
+print(0 | 1)
+print(1 | 0)
+print(1 | 1)
+
+print('Bitwise ExOR operator')
+print(0 ^ 0)
+print(0 ^ 1)
+print(1 ^ 0)
+print(1 ^ 1)
+
+print('<< Binary Left Shift')
+a = 240
+print(a << 2)
+
+print('<< Binary Right Shift')
+a = 24
+print(a << 2)
+
+print('~ Binary Ones Complement')
+a = 60
+print(~a)
---
+++ Day1/Operators/comparision.py
@@ -0,0 +1,26 @@
+# Operators:
+# Arithmetic
+# Comparision
+# Logical
+# Membership
+# Identity
+# Bitwise
+
+# Comparision operators
+print('----------Comparision-------')
+
+i = 10
+j = 20
+print(i > j)
+print(i < j)
+print(i >= j)
+print(i <= j)
+print(i == j)
+print(i != j)  # Not equal
+# print(i<>j) # Not equal, not supported in python v3.X
+
+
+
+
+
+
