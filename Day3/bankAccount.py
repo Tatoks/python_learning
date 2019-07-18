961c8cb85e1c4930ee471d54bf94f9e88b5b9841
Abhilash Sanne <sanne.abhi@gmail.com>
2019-07-18 16:49:31 +0530

    Add day3 files

---
+++ Day3/bankAccount.py
@@ -0,0 +1,51 @@
+# Without inheritance
+
+class BankAccount:
+
+    def __init__(self, custId, custName, custPhone, custEmail, accType, accBal, accNumber):
+        self.id = custId
+        self.name = custName
+        self.phone = custPhone
+        self.email = custEmail
+        self.accType = accType
+        self.bal = accBal
+        self.accNum = accNumber
+
+    def balanceEnquiry(self):
+        print("Customer Id: ", self.id)
+        print("Customer name: ", self.name)
+        print("Customer phone: ", self.phone)
+        print("Customer email: ", self.email)
+        print("Customer accType: ", self.accType)
+        print("Customer bal: ", self.bal)
+        print("Customer accNum: ", self.accNum)
+
+    def deposit(self, amount):
+        self.bal = self.bal + amount
+        print("New updated balance is:", self.bal)
+
+    def withdraw(self, amount):
+        if self.bal > amount:
+            self.bal = self.bal - amount;
+            print("Updated balance amount is: ", self.bal)
+        else:
+            print('Insufficient Balance:', self.bal)
+
+    def savingsAccount(bankAccount):
+        print("Rate of interest : 6.8%")
+        print("Daily deposit amount limit: 100000/-")
+        print("Daily withdrawal amount limit: 20000/-")
+        print("Minimum balance: 3000/-")
+
+    def currentAccount(bankAccount):
+        print("Rate of interest : 0.0%")
+        print("Daily deposit amount limit: No Limit")
+        print("Daily withdrawal amount limit: No Limit")
+        print("Minimum balance: 100000/-")
+
+#custId, custName, custPhone, custEmail, accType, accBal, accNumber
+cust1 = BankAccount(1001, 'Abhilash', 920930929, 'abhi@inno,com', 'saving', 10000, 13898298398)
+cust1 = BankAccount(1002, 'Sanne', 920930928, 'san@inno,com', 'current', 10000, 13898298398)
+
+cust1.balanceEnquiry()
+cust1.currentAccount()
---
+++ Day3/polymorphism.py
@@ -0,0 +1,65 @@
+class Animal:
+    def whoAmI(self):
+        print("I am Animal")
+
+    def eat(self):
+        print("Animal is eating")
+
+    @staticmethod
+    def walk():
+        print('Animal is walking')
+
+
+class Dog(Animal):
+    def __init__(self):
+        print("Dog created")
+
+    # Method overriding
+    def whoAmI(self):
+        print("I am Dog")
+
+    # dog extending base class functionality
+    def bark(self):
+        print("Bow Bow!!")
+
+
+class Cat(Animal):
+    def __init__(self):
+        print("Cat Created")
+
+    # Method overriding
+    def whoAmI(self):
+        print("I am Cat")
+
+    # cat extending base class functionality
+    def bark(self):
+        print("Meow Meow!!")
+    # End of class Cat
+
+
+# polymorphism is dynamic binding technique
+def func(obj):
+    obj.walk()
+    obj.eat()
+
+
+print('-------------------------------------')
+a = Animal()
+d= Dog()
+c = Cat()
+print('-------------------------------------')
+a.whoAmI()
+d.whoAmI()
+c.whoAmI()
+print('-------------------------------------')
+a.eat()
+d.eat()
+c.eat()
+print('-------------------------------------')
+a.walk()
+d.walk()
+c.walk()
+print('-------------------------------------')
+func(a)
+func(d)
+func(c)
