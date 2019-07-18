0bd1d19d9fc65a31e85914371c2852de4c944aee
Abhilash Sanne <sanne.abhi@gmail.com>
2019-07-18 16:48:41 +0530

    Add Day2\*.py and Assignments

---
+++ Day2/Assignments/grade.py
@@ -0,0 +1,21 @@
+# Calculate grade using nested dictionaries
+
+student_record={1000:{'name':'A','M':50,'P':60,'C':70},1001:{'name':'B','M':55,'P':65,'C':75}}
+
+for key,value in student_record.items():
+    print('****************************')
+    print('ID: '+str(key))
+    total_marks = 0
+    for k1,v1 in value.items():
+        if k1.casefold()=='name':
+            print(k1.capitalize()+': '+v1)
+        if (k1=='M') or (k1=='P') or (k1=='C'):
+            total_marks = total_marks + v1
+    average_scre = total_marks / 3
+    if average_scre >= 75:
+        print('Grade A')
+    elif average_scre >= 65 and average_scre < 75:
+        print('Grade B')
+    else:
+        print('Grade C')
+print('****************************')
---
+++ Day2/Dictionaries.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Key Value Pairs
+mydict = {'a': 'apple',
+          'b': 'ball',
+          'c': 'cat',
+          'd': 'dog'}
+print(mydict)
+print(mydict['a'])
+
+mydict['a'] = 'Avacado'  # update value for key 'a'
+print(mydict)
+
+del mydict['b']  # Delete 'b' key value pair
+print(mydict)
+
+# Below line deletes entire dictionary
+# del mydict
+# print(mydict)
+
+for i in mydict:
+    print(i, ":", mydict[i])
+
+print("-------Use Items() method-------")
+for key, value in mydict.items():
+    print(key, value)
+
+# Print length of dictionary
+print(len(mydict))
+
+# Multi dimensional dictionaries
+
+students = {101: {'name': 'John', 'maths': 50, 'chemistry': 56, 'physics': 75},
+            102: {'name': 'Steve', 'maths': 66, 'chemistry': 89, 'physics': 59},
+            103: {'name': 'Peter', 'maths': 88, 'chemistry': 78, 'physics': 86},
+            104: {'name': 'Ben', 'maths': 99, 'chemistry': 76, 'physics': 99}
+            }
+
+for student in students:
+    print(students[student]['name'], end=",")
+
+print()
+print('-----------------------------------------')
+
+for key, value in students.items():
+    print(key, end=":")
+    for k, v in value.items():
+        #         print(k, v, end=',')
+        print(v)
+    print("")
